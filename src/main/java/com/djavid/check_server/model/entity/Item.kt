6e1fbf6ee6db79864ab434d1ed1f89b6c578a999
djavid <dpkhalilov@edu.hse.ru>
2018-04-10 22:24:52 +0300

    Deploy

---
+++ src/main/java/com/djavid/check_server/model/entity/Item.kt
@@ -0,0 +1,18 @@
+package com.djavid.check_server.model.entity
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class Item(
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        val quantity: Long,
+        val price: Long,
+        val sum: Long,
+        val name: String,
+        val nds18: Long,
+        val nds10: Long
+)
