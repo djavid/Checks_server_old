cefefe46b63353fc585197e28466c396d4f22928
djavid <dpkhalilov@edu.hse.ru>
2018-04-10 21:58:48 +0300

    Deploy

--- src/main/java/com/djavid/check_server/ChecksApplication.java
+++ src/main/java/com/djavid/check_server/ChecksApplication.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ChecksApplication {
@@ -12,5 +15,10 @@ public class ChecksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChecksApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 }
