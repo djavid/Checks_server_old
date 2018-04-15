a4aa6365e858a17cf1a458668f1673f153eea7a1
djavid <dpkhalilov@edu.hse.ru>
2018-04-15 20:38:46 +0300

    kg

--- src/main/java/com/djavid/check_server/model/repository/ItemRepository.java
+++ src/main/java/com/djavid/check_server/model/repository/ItemRepository.java
@@ -2,7 +2,9 @@ package com.djavid.check_server.model.repository;
 
 import com.djavid.check_server.model.entity.Item;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends CrudRepository<Item, Long> {
 
     Item findItemById(Long id);
--- src/main/java/com/djavid/check_server/model/repository/ReceiptRepository.java
+++ src/main/java/com/djavid/check_server/model/repository/ReceiptRepository.java
@@ -2,7 +2,9 @@ package com.djavid.check_server.model.repository;
 
 import com.djavid.check_server.model.entity.Receipt;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReceiptRepository extends CrudRepository<Receipt, Long> {
 
     Receipt findReceiptById(Long id);
