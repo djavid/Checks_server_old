5462107a5d2a423e679ec5c963bb7bcb1d034886
djavid <dpkhalilov@edu.hse.ru>
2018-04-16 14:23:21 +0300

    Deploy

--- src/main/java/com/djavid/checkserver/model/repository/ItemRepository.java
+++ src/main/java/com/djavid/checkserver/model/repository/ItemRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ItemRepository extends CrudRepository<Item, Long> {
 
-    Item findItemByitem_id(Long id);
+    Item findItemByItem_id(Long id);
 
 }
