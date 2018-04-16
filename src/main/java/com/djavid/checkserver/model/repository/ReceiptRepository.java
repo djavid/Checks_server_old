9996bad1abae128ddded2b1f1b3c177973c17ce4
djavid <dpkhalilov@edu.hse.ru>
2018-04-16 14:21:15 +0300

    Deploy

--- src/main/java/com/djavid/checkserver/model/repository/ReceiptRepository.java
+++ src/main/java/com/djavid/checkserver/model/repository/ReceiptRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReceiptRepository extends CrudRepository<Receipt, Long> {
 
-    Receipt findReceiptByreceipt_id(Long id);
+    Receipt findReceiptByReceipt_id(Long id);
     Receipt findReceiptByShop(String user);
 
 }
