9f29b6efdee74e1b34c3bbfeeaa3703dc0f1d53a
djavid <dpkhalilov@edu.hse.ru>
2018-05-16 06:50:36 +0300

    Deploy

--- src/main/java/com/djavid/checkserver/ServerConfiguration.java
+++ src/main/java/com/djavid/checkserver/ServerConfiguration.java
@@ -1,55 +1,40 @@
 package com.djavid.checkserver;
 
 import com.djavid.checkserver.model.api.Api;
-import com.google.gson.Gson;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import okhttp3.logging.HttpLoggingInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 @Configuration
 public class ServerConfiguration {
 
     @Bean
     public OkHttpClient okHttpClient() {
-        OkHttpClient client = new OkHttpClient();
-        Logger logger = LoggerFactory.getLogger("LoggingInterceptor");
 
-        client.interceptors().add(chain -> {
-            Request request = chain.request();
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
-            long t1 = System.nanoTime();
-            logger.info(String.format("Sending request %s on %s%n%s",
-                    request.url(), chain.connection(), request.headers()));
-
-            Response response = chain.proceed(request);
-
-            long t2 = System.nanoTime();
-            logger.info(String.format("Received response for %s in %.1fms%n%s",
-                    response.request().url(), (t2 - t1) / 1e6d, response.headers()));
-
-            return response;
-        });
-        return client;
+        return new OkHttpClient.Builder().addInterceptor(loggingInterceptor).build();
     }
 
     @Bean
-    public Retrofit retrofit(OkHttpClient client, Gson gson) {
+    public Retrofit retrofit(OkHttpClient client) {
 
         return new Retrofit.Builder()
-                //.addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                //.addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl("https://predictcheck.herokuapp.com")
                 .client(client)
                 .build();
     }
 
     @Bean
-    public Api randomNumberService(Retrofit retrofit) {
+    public Api apiService(Retrofit retrofit) {
         return retrofit.create(Api.class);
     }
 }
