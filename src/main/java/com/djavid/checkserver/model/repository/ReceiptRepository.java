fa0ca909a10e5a47122f95af034354e5ee7878c8
djavid <dpkhalilov@edu.hse.ru>
2018-05-19 16:56:35 +0300

    Deploy

--- src/main/java/com/djavid/checkserver/model/repository/ReceiptRepository.java
+++ src/main/java/com/djavid/checkserver/model/repository/ReceiptRepository.java
@@ -12,6 +12,6 @@ public interface ReceiptRepository extends CrudRepository<Receipt, Long> {
     Receipt findReceiptByReceiptId(Long id);
     Receipt findReceiptByUser(String user);
     List<Receipt> findReceiptsByTokenId(Long tokenId);
-    boolean existsByFiscalDriveNumberAndFiscalDocumentNumberAndFiscalSignAndEmptyIs
+    boolean existsByFiscalDriveNumberAndFiscalDocumentNumberAndFiscalSignAndEmpty
             (String fiscalDriveNumber, String fiscalDocumentNumber, String fiscalSign, boolean isEmpty);
 }
